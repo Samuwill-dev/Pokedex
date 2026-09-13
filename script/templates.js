afc7bcfdcad51f41dc3488671702ac531fce7150
Samu <94405809+MarioCode21@users.noreply.github.com>
2026-09-13 13:26:08 +0200

    first template added

--- script/templates.js
+++ script/templates.js
@@ -1,1 +1,14 @@
-// hier templates, also funktionen die HTML code in sich haben
+
+function getcard(params) {
+    return `
+    <div class="card" data-id="card">
+            <h3></h3>
+            <div>
+                <div class="type">
+                    wasser, fliegen 
+                </div>
+                <img src="" alt="name pokemon">
+            </div>
+        </div>
+    `
+}
