49821f52dac2ffa09c21a633b6563f3d69908d6c
Samu <94405809+MarioCode21@users.noreply.github.com>
2026-09-16 15:59:59 +0200

    fetch names

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
-// hier das hauptscript
+async function fetchData(id) {
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    let apidata = await response.json();
+    console.log(apidata);
+
+    let indexFromApiData = Object.keys(apidata.results)
+    console.log(indexFromApiData);
+
+    for (let indexFromApiData = 0; indexFromApiData < apidata.results.length; indexFromApiData++) {
+        console.log(apidata.results[indexFromApiData]);
+        let pokemonName = apidata.results[indexFromApiData].name
+        console.log(pokemonName);
+        
+    }
+}
