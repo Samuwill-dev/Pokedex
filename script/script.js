9bf53dca8d0a115494752ee692444a309e3411e9
Samu <94405809+MarioCode21@users.noreply.github.com>
2026-09-17 16:40:54 +0200

    pokemon in html laden

--- script/script.js
+++ script/script.js
@@ -9,18 +9,18 @@ async function fetchData(id) {
     let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
     let apidata = await response.json();
     
-
     //name holen
     let indexFromApiData = Object.keys(apidata.results)
     
     for (let indexFromApiData = 0; indexFromApiData < apidata.results.length; indexFromApiData++) {
         let pokemonName = apidata.results[indexFromApiData].name
-        
-        restData(pokemonName)
+
+        await loadData(pokemonName)
     }
+    
 }
 
-async function restData(name) {
+async function loadData(name) {
     let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
     let apidata = await response.json();
 
@@ -29,10 +29,14 @@ async function restData(name) {
     let pokemonTypes = apidata.types.map(typeInfo => typeInfo.type.name)
     let pokemonImg = apidata.sprites.front_default
 
+    // fügt daten in array ein
     loadedPokemon.pokemonId.push(pokemonId)
     loadedPokemon.pokemonName.push(pokemonName)
     loadedPokemon.pokemonType.push(pokemonTypes)
     loadedPokemon.pokemonImg.push(pokemonImg)
 
     console.log(loadedPokemon);
+    document.getElementById("card-section").innerHTML += getcard(pokemonId, pokemonName, pokemonTypes, pokemonImg)
 }
+
+
--- script/templates.js
+++ script/templates.js
@@ -1,13 +1,13 @@
 
-function getcard(params) {
+function getcard(id, name, type, img) {
     return `
     <div class="card" data-id="card">
-            <h3></h3>
+            <h3>${id}   ${name}</h3>
             <div>
                 <div class="type">
-                    wasser, fliegen 
+                     ${type}
                 </div>
-                <img src="" alt="name pokemon">
+                <img src="${img}" alt="image form ${name}">
             </div>
         </div>
     `
