13118c885a07e2fcbeb3defae1f06c553e4c1027
Samu <94405809+MarioCode21@users.noreply.github.com>
2026-09-17 15:42:52 +0200

    load data in array from api

--- script/script.js
+++ script/script.js
@@ -1,15 +1,38 @@
+let loadedPokemon = {
+    'pokemonId' : [],
+    'pokemonName' : [],
+    'pokemonType' : [],
+    'pokemonImg' : []
+}
+
 async function fetchData(id) {
-    let response = await fetch('https://pokeapi.co/api/v2/pokemon/')
+    let response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=20&offset=0')
     let apidata = await response.json();
-    console.log(apidata);
+    
 
+    //name holen
     let indexFromApiData = Object.keys(apidata.results)
-    console.log(indexFromApiData);
-
+    
     for (let indexFromApiData = 0; indexFromApiData < apidata.results.length; indexFromApiData++) {
-        console.log(apidata.results[indexFromApiData]);
         let pokemonName = apidata.results[indexFromApiData].name
-        console.log(pokemonName);
         
+        restData(pokemonName)
     }
+}
+
+async function restData(name) {
+    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    let apidata = await response.json();
+
+    let pokemonId = apidata.id
+    let pokemonName = apidata.name
+    let pokemonTypes = apidata.types.map(typeInfo => typeInfo.type.name)
+    let pokemonImg = apidata.sprites.front_default
+
+    loadedPokemon.pokemonId.push(pokemonId)
+    loadedPokemon.pokemonName.push(pokemonName)
+    loadedPokemon.pokemonType.push(pokemonTypes)
+    loadedPokemon.pokemonImg.push(pokemonImg)
+
+    console.log(loadedPokemon);
 }
