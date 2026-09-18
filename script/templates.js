82fd94ea0a2821ee52215ce0b142d4c6950af491
Samu <94405809+MarioCode21@users.noreply.github.com>
2026-09-18 10:23:27 +0200

    header and footer from card styleed

--- script/templates.js
+++ script/templates.js
@@ -2,12 +2,11 @@
 function getcard(id, name, type, img) {
     return `
     <div class="card" data-id="card">
-            <h3>${id} ${name}</h3>
-            <div>
-                <div class="type">
-                     ${type}
-                </div>
-                <img class="pokemon-img" src="${img}" alt="image form ${name}">
+            <h3 class="header">${id} ${name}</h3>
+            
+            <img class="pokemon-img" src="${img}" alt="image form ${name}">
+            <div class="type">
+                ${type}
             </div>
         </div>
     `
